6352f9f0825c14ea704a96fe5103daf37606c9c7
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-08-19 14:31:10 -0300

    Telephone Number Validator option 2

--- 4-TelephoneValidator.js
+++ 4-TelephoneValidator.js
@@ -1,17 +1,23 @@
 function telephoneCheck(str) {
-  if (str[0] === "(" && str[str.length - 1] === ")") return false;
-  if (str.includes("(") && !str.includes(")")) return false;
-  if (!str.includes("(") && str.includes(")")) return false;
-  if (str.includes("?")) return false;
-  if (str[0] === "-") return false;
-  if ((str.match(/-/g) || []).length >= 3) return false;
-  let cleanStr = str.split(/[\W_]/g).join("");
-  if (cleanStr.length > 11) return false;
-  if (cleanStr.length < 10) return false;
-  if (cleanStr.length === 11 && cleanStr[0] !== "1") return false;
+  let regExp = /^(1\s?)?(\d{3}|\(\d{3}\))[\-\s]?\d{3}[\-\s]?\d{4}$/;
 
-  return true;
+  return regExp.test(str);
 }
+
+// function telephoneCheck(str) {
+//   if (str[0] === "(" && str[str.length - 1] === ")") return false;
+//   if (str.includes("(") && !str.includes(")")) return false;
+//   if (!str.includes("(") && str.includes(")")) return false;
+//   if (str.includes("?")) return false;
+//   if (str[0] === "-") return false;
+//   if ((str.match(/-/g) || []).length >= 3) return false;
+//   let cleanStr = str.split(/[\W_]/g).join("");
+//   if (cleanStr.length > 11) return false;
+//   if (cleanStr.length < 10) return false;
+//   if (cleanStr.length === 11 && cleanStr[0] !== "1") return false;
+
+//   return true;
+// }
 
 console.log(telephoneCheck("555-555-5555")); //true.
 // console.log(telephoneCheck("1 555-555-5555")); //true.
